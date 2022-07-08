5910ad67fc571e14a68c73998ece3fa3a573ab21
camilogranda <camilo.granda96@gmail.com>
2022-07-07 21:53:55 -0500

    más comandos de git

--- main.c
+++ main.c
@@ -11,4 +11,6 @@ int main (void)
     //git checkout HEAD -- <archivo a reiniciar al último commit>
     //git add <archivo a agregar>
     //git log -> historia de commits
+    //git log --oneline -> historia reducida de commits
+    //git log --oneline --graph -> historia con gráfico de commits
 }
