38ed761be86c48a6c30d434fec2dc90b1fbfa987
camilogranda <camilo.granda96@gmail.com>
2022-07-07 19:40:47 -0500

    git commands

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
 int main (void)
 {
     printf("Hola Mundo");
+
+    //git clone <repo>
+    //git fetch
+    //git pull origin main
+    //git push origin main
+    //git commit -am "mensaje"
+    //cd .. -> devolverse de carpeta
 }
